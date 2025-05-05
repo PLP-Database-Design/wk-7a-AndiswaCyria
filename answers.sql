3b7bb9907561c3e51a62f8d3e0ed637de8bc02b0
Andiswa Molangathi <andiswacyriam@gmail.com>
2025-05-05 22:36:44 +0200

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -5,81 +5,22 @@
 CREATE TABLE ProductDetail (
     OrderID INT,
     CustomerName VARCHAR(100),
-    Products VARCHAR(255)
+    Products VARCHAR(100)
 );
 
 INSERT INTO ProductDetail (OrderID, CustomerName, Products) VALUES
-(101, 'John Doe', 'Laptop, Mouse'),
-(102, 'Jane Smith', 'Tablet, Keyboard, Mouse'),
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102,'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
 (103, 'Emily Clark', 'Phone');
-
--- 1NF Normalized --
-
--- Orders Table -- 
-
-CREATE TABLE Orders (
-  OrderID INT, 
-  CustomerName VARCHAR(100),
-);
-
--- OrderProducts Table -- 
-Create TABLE OrderProducts (
-  OrderID INT, 
-  Product VARCHAR(100)
-  PRIMARY KEY (OrderID, Product),
-  FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
-);
-
--- SQL QUERY -- 
-
---Orders Table -- 
-INSERT INTO Orders (OrderID, CustomerName)
-SELECT DISTINCT
-    OrderID,
-    CustomerName
-FROM
-    ProductDetail;
-
--- OrderProducts Table 
-
--- For OrderID 101 (John Doe)
-INSERT INTO OrderProducts (OrderID, Product)
-VALUES
-(101, 'Laptop'),
-(101, 'Mouse');
-
--- For OrderID 102 (Jane Smith)
-INSERT INTO OrderProducts (OrderID, Product)
-VALUES
-(102, 'Tablet'),
-(102, 'Keyboard'),
-(102, 'Mouse');
-
--- For OrderID 103 (Emily Clark)
-INSERT INTO OrderProducts (OrderID, Product)
-VALUES
-(103, 'Phone');
-
 
 
 -- Question 2 -- 
 
 -- Original Table -- 
 
-CREATE TABLE OrderDetails (
-    OrderID INT,
-    CustomerName VARCHAR(100),
-    Product VARCHAR(100),
-    Quantity INT
-);
-
-INSERT INTO OrderDetails (OrderID, CustomerName, Product, Quantity) VALUES
-(101, 'John Doe', 'Laptop', 2),
-(101, 'John Doe', 'Mouse', 1),
-(102, 'Jane Smith', 'Tablet', 3),
-(102, 'Jane Smith', 'Keyboard', 1),
-(102, 'Jane Smith', 'Mouse', 2),
-(103, 'Emily Clark', 'Phone', 1);
 
 -- 2NF Query -- 
 
@@ -90,26 +31,24 @@ CREATE TABLE Orders (
 );
 
 INSERT INTO Orders (OrderID, CustomerName)
-SELECT DISTINCT
-    OrderID,
-    CustomerName
-FROM
-    OrderDetails;
+VALUES 
+    (101, 'John Doe'),
+    (102, 'Jane Smith'),
+    (103, 'Emily Clark');
 
+CREATE TABLE Product(
+    OrderID INT,
+    Product VARCHAR(100),
+    Quantity INT,
+    PRIMARY KEY (OrderID, Product),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
 
-CREATE TABLE OrderItems (
-  OrderID INT, 
-  Product VARCHAR(100),
-  Quantity INT,
-  PRIMARY KEY (OrderID, Product),
-  FOREIGN KEY(OrderID) REFERENCES Orders(OrderID)
-  );
+INSERT INTO Product (OrderID, Product, Quantity)
+    VALUES
+    (101, 'Laptop', 2),
+    (101, 'Mouse',1),
+    (102, 'Tablet',3),
+    (103, 'Phone', 1);
 
-INSERT INTO OrderItems (OrderID, Product, Quantity)
-SELECT
-    OrderID,
-    Product,
-    Quantity
-FROM
-    OrderDetails;
 
